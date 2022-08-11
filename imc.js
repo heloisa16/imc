bbd1f53f8bc7bd25dc93c687bbf9ce9a8e29ce07
HELO <94690513+heloisa16@users.noreply.github.com>
2022-08-11 09:22:06 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -1,6 +1,6 @@
 const calcular = document.getElementById('calcular');
 
-function imc()/*{"O imc do meu amigo é " + imcAmigo); function calculaImc() var imc = peso / (altura* altura)*/
+function imc(){ /*{"O imc do meu amigo é " + imcAmigo); function calculaImc() var imc = peso / (altura* altura)*/
     const altura = document.getElementById('altura').value;/* serve para retornar um elemento do DOM que é identificado por um ID específico.*/
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
@@ -9,7 +9,7 @@ function imc()/*{"O imc do meu amigo é " + imcAmigo); function calculaImc() var
     const valorIMC = (peso / (altura * altura)).toFixed(1);
 
     let classificacao = '';/*é chamado de linguagem Cascading Style Sheet e é usado para estilizar elementos escritos em uma linguagem de marcação como HTML*/
-    if /* pode ser usado para estilizar um documento muito básico de texto */(valorIMC < 18.5){
+    if (valorIMC < 18.5){ /* pode ser usado para estilizar um documento muito básico de texto */
         classificacao = 'abaixo do peso';
     } else if(valorIMC <25){
           classificacao = 'peso ideal';
